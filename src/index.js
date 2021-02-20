efb94a4852632cd27347e3fd1430b015bf801551
John Davis <johnmdavis125@gmail.com>
2021-02-20 06:43:41 -0500

    finished up section 3  - props mini app

--- src/index.js
+++ src/index.js
@@ -8,23 +8,33 @@ const App = () => {
     return (
       <div className="ui container comments">
           <ApprovalCard>
+              <div>
+                <h4>Warning</h4>
+                Are you sure you want to do this?
+              </div>
+          </ApprovalCard>
+          
+          <ApprovalCard>
             <CommentDetail 
                 author="Sam"
                 timeAgo="Today at 4:45"
                 body="Ignorant rant"
                 avatar={faker.image.image()} />
           </ApprovalCard>
-          <CommentDetail
-            author="Alex"
-            timeAgo="Today at 2:00"
-            body="snarky comment"
-            avatar={faker.image.image()}/>
-          
-          <CommentDetail
-            author="Jane"
-            timeAgo="Today at 5:00"
-            body="props aint nothin but a thing"
-            avatar={faker.image.image()}/>
+          <ApprovalCard>
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 2:00"
+                body="snarky comment"
+                avatar={faker.image.image()}/>
+          </ApprovalCard>
+          <ApprovalCard>  
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 5:00"
+                body="props aint nothin but a thing"
+                avatar={faker.image.image()}/>
+          </ApprovalCard>
       </div>  
     );
 };
